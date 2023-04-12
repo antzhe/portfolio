8f31f039821b78a34ce199b61f1d5de78ea98506
antzhe <anton.zhelomsky@yandex.ru>
2023-04-12 20:53:39 +0500

    add string calculator

---
+++ stringCalculator.py
@@ -0,0 +1,58 @@
+def rpn(expression):
+    precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
+    stack = []
+    output = []
+    for char in expression:
+        if char.isalnum():
+            output.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            output.append(" ")
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        else:
+            output.append(" ")
+            while stack and stack[-1] != '(' and precedence[char] <= precedence[stack[-1]]:
+                output.append(stack.pop())
+                output.append(" ")
+            stack.append(char)
+    while stack:
+        output.append(" ")
+        output.append(stack.pop())
+    return ''.join(output)
+
+math_str = input()
+RPN = rpn(math_str)
+result = []
+for i in RPN.split():
+    if i.isdigit():
+        result.append(int(i))
+    else:
+        if i == '+':
+            b = result.pop()
+            a = result.pop()
+            res = a + b
+            result.append(res)
+        elif i == '-':
+            b = result.pop()
+            a = result.pop()
+            res = a - b
+            result.append(res)
+        elif i == '*':
+            b = result.pop()
+            a = result.pop()
+            res = a * b
+            result.append(res)
+        elif i == '/':
+            b = result.pop()
+            a = result.pop()
+            res = a / b
+            result.append(res)
+        elif i == '^':
+            b = result.pop()
+            a = result.pop()
+            res = a**b
+            result.append(res)
+print(int(result[0]))
